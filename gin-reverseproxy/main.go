82d4c1c5e8c700ceb6110c099eaddc2d45996943
Sébastien Le Gall <s.legall@meetic-corp.com>
2020-06-23 12:22:10 +0200

    Add reverse proxy with Gin

---
+++ gin-reverseproxy/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+
+	"github.com/gin-gonic/gin"
+)
+
+func proxy(c *gin.Context) {
+	remote, err := url.Parse("http://myremotedomain.com")
+	if err != nil {
+		panic(err)
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(remote)
+	proxy.Director = func(req *http.Request) {
+		req.Header = c.Request.Header
+		req.Host = remote.Host
+		req.URL.Scheme = remote.Scheme
+		req.URL.Host = remote.Host
+		req.URL.Path = c.Param("proxyPath")
+	}
+
+	proxy.ServeHTTP(c.Writer, c.Request)
+}
+
+func main() {
+
+	r := gin.Default()
+
+	r.Any("/*proxyPath", proxy)
+
+	r.Run(":8080")
+}
